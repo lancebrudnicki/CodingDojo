ca7a28a106cb6f175a0af8cce52a75bec42621fb
lancebrudnicki <lancebrudnicki@gmail.com>
2021-05-18 11:56:13 -0600

    main

---
+++ values.js
@@ -0,0 +1,14 @@
+
+var arr = [1,3,5,7,9,13]
+function valGreaterThanSecond(arr)
+        {
+        var newarr= []
+            for( var i =0; i< arr.length;i++)
+            {
+            if( arr[i]> arr[3] )
+                newarr.push(i) 
+            }
+        console.log(arr[i]);
+    }
+
+console.log(valGreaterThanSecond)
